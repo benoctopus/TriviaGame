d97d50f3538c4f54de6a0ed934f3b04ff568adf1
Benjamin <rose.benjamin925@gmail.com>
2018-03-26 14:43:28 -0700

    added some html

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// const queryUrl = "https://opentdb.com/api.php?";
+//
+// class Trivia {
+//   constructor(category, amount) {
+//     $.ajax({
+//       url: `https://opentdb.com/api.php?amount=${amount}&category=${category}`,
+//       method: "get",
+//     }).then(function(response) {
+//       console.log(response);
+//       window.b = response;
+//     })
+//   }
+//
+// }
+
+// $(document).ready($(document.body).fadeIn(1000));
+
+
+
+// let a = new Trivia("18", "4");
