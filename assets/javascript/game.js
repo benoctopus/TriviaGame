5dc7fd7da794720ef14d4af9870c011c56b563b4
Benjamin <rose.benjamin925@gmail.com>
2018-03-29 02:38:49 -0700

    clock is foolproof

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,16 +10,28 @@ class Clock {
 
   set() {
     this.clockRunning = true;
-    this.clockElement.text(this.currentTime)
-    this.clockInterval = setInterval(this.decrement, 1000)
+    this.clockElement.text(`Time Remaining: ${this.currentTime}`);
+    this.clockRow.css("display", "block");
+    this.clockInterval = setInterval(this.decrement.bind(this), 1000)
   }
 
   decrement() {
     if (this.currentTime < 1) {
       clearInterval(this.clockInterval);
+      $("#question").text("OUT OF TIME");console.log("correct");
+      $("#answer-feild").fadeOut(750, function () {
+        gameEnv.number++;
+        $("#number").text(`#: ${gameEnv.number}`);
+        gameEnv.incorrect++;
+        gameEnv.roundIncorrect++;
+        gameEnv.loadQuestion();
+      });
+      $("li").removeEventListener();
     }
+
     this.currentTime--;
-    this.clockElement.text(this.currentTime);
+    console.log(this);
+    this.clockElement.text(`Time Remaining: ${this.currentTime.toString()}`);
   }
 
 }
@@ -30,10 +42,12 @@ window.gameEnv = {
   startMenu: $("#startup"),
   triviaQue: [],
   triviaApi: "https://opentdb.com/api.php?",
-  imgApi: "https://api.unsplash.com/?" +
-  "client_id=9c65426339aa4c584d1c2dd8b160cbfa502cf5d143c111141de73600bbb0daca" +
-  "&w=1920" +
-  "&h=1080",
+
+  //implement later
+  // imgApi: "https://api.unsplash.com/?" +
+  // "client_id=9c65426339aa4c584d1c2dd8b160cbfa502cf5d143c111141de73600bbb0daca" +
+  // "&w=1920" +
+  // "&h=1080",
 
   display: [
     //initial display
@@ -53,31 +67,32 @@ window.gameEnv = {
     this.display[0]();
   },
 
-  getOptions: function() {
-    let defaults = ["questions", "difficulty", "category"];
-    let amount, difficulty, category;
-    let complete = true;
-    $.each($("select option:selected"), function (index, value) {
-      console.log(defaults.indexOf(value.text));
-      if (defaults.indexOf(value.text) > -1 && complete) {
-        alert("Complete setup to continue");
-        complete = false;
-      }
-      else if (value.value === "dif" && complete) {
-        difficulty = value.text;
-      }
-      else if (value.value === "amount" && complete) {
-        amount = value.text;
-      }
-      else if (value.value.split("-")[0] === "cat" && complete) {
-        category = value.value.split("-").pop();
-      }
-    });
-    if (!complete) {
-      return;
-    }
-    this.getTrivia(category, amount, difficulty)
-  },
+  // implement later
+  // getOptions: function() {
+  //   let defaults = ["questions", "difficulty", "category"];
+  //   let amount, difficulty, category;
+  //   let complete = true;
+  //   $.each($("select option:selected"), function (index, value) {
+  //     console.log(defaults.indexOf(value.text));
+  //     if (defaults.indexOf(value.text) > -1 && complete) {
+  //       alert("Complete setup to continue");
+  //       complete = false;
+  //     }
+  //     else if (value.value === "dif" && complete) {
+  //       difficulty = value.text;
+  //     }
+  //     else if (value.value === "amount" && complete) {
+  //       amount = value.text;
+  //     }
+  //     else if (value.value.split("-")[0] === "cat" && complete) {
+  //       category = value.value.split("-").pop();
+  //     }
+  //   });
+  //   if (!complete) {
+  //     return;
+  //   }
+  //   this.getTrivia(category, amount, difficulty)
+  // },
 
   generateParameters: function() {
     let parameters = [];
@@ -186,105 +201,59 @@ window.gameEnv = {
     }
   },
 
-  startClock: function (time) {
-    this.clock = {
-    };
-    this.clockInterval = setInterval(function (time) {
-      if (gameEnv.clock.clockRunning === false) {
-      }
-
-
-
-    });
-  },
-
   loadQuestion: function() {
     if (typeof this.triviaQue[0].questions[0] === "undefined") {
+      clearInterval(gameEnv.clock.clockInterval);
       this.round++;
       this.triviaQue.splice(0, 1);
       this.ajaxWait();
     }
-    this.setAnswers();
-    // this.current = this.triviaQue[0].questions[0];
-    // let ansSet = $(".answer");
-    // let i = 0;
-    // let j = 0;
-    // let order = this.shuffle([1, 2, 3, 4]);
-    // this.current.incorrect_answers = this.shuffle(this.current.incorrect_answers);
-    // if (this.current.type === "boolean") {
-    //   $("#ans-1").text("True");
-    //   $("#ans-2").text("False");
-    //   if (gameEnv.current.correct_answer === "True") {
-    //     $("#ans-1").attr("data-win", "win");
-    //     $("#ans-2").attr("data-win", "loose")
-    //   }
-    //   else {
-    //     $("#ans-2").attr("data-win", "win");
-    //     $("#ans-1").attr("data-win", "loose")
-    //   }
-    //   $("#ans-3, #ans-4").css("display", "none");
-    //   $("#answer-feild").fadeIn(250);
-    // }
-    // else {
-    //   ansSet.each(function () {
-    //     ansSet.css("display", "block");
-    //     ansSet.css("display", "list-item");
-    //     console.log($(this));
-    //     if (order[i] === 1) {
-    //       $(this).text(
-    //         htmlFixer(
-    //           gameEnv.current.correct_answer)
-    //       ).attr("data-win", "win");
-    //     }
-    //     else{
-    //       $(this).text(
-    //         htmlFixer(
-    //           gameEnv.current.incorrect_answers[j])
-    //       ).attr("data-win", "loose");
-    //       j++
-    //     }
-    //     i++;
-    //   });
-    //   $("#answer-feild").fadeIn(250);
-    // }
-    gameEnv.triviaQue[0].questions.splice(0,1);
-    //win/loose
-    $("#question").text(htmlFixer(gameEnv.current.question));
-    $("li").on("click", function () {
-      let cAns = $(this);
-      console.log(cAns.attr("data-win"));
-      if (cAns.attr("data-win") === "win") {
-        gameEnv.correct++;
-        gameEnv.roundCorrect++;
-        cAns.attr("data-win", "");
-        $("#question").text("Correct");
-        cAns.addClass("correct-answer");
-        setTimeout(function () {
-          cAns.removeClass("correct-answer")
-        }, 500);
-      }
-      else if (cAns.attr("data-win") === "loose") {
-        cAns.attr("data-win", "");
-        gameEnv.incorrect++;
-        gameEnv.roundIncorrect++;
-        $("#question").text("Wrong!");
-        cAns.addClass("wrong-answer");
-        setTimeout(function () {
-          cAns.removeClass("wrong-answer")
-        }, 500);
-      }
-      else {
-        return
-      }
-      gameEnv.number++;
-      $("#number").text(`#: ${gameEnv.number}`);
-      console.log("correct");
-      $("#answer-feild").fadeOut(500, function () {
-        cAns.removeClass("correct-answer wrong-answer");
-        gameEnv.loadQuestion();
+    else {
+      this.setAnswers();
+      // clearInterval(this.clock.clockInterval);
+      this.clock = new Clock(7);
+      gameEnv.triviaQue[0].questions.splice(0, 1);
+
+      //win/loose
+      $("#question").text(htmlFixer(gameEnv.current.question));
+      $("li").on("click", function () {
+        let cAns = $(this);
+        console.log(cAns.attr("data-win"));
+        if (cAns.attr("data-win") === "win") {
+          gameEnv.correct++;
+          gameEnv.roundCorrect++;
+          cAns.attr("data-win", "");
+          $("#question").text("Correct");
+          cAns.addClass("correct-answer");
+          setTimeout(function () {
+            cAns.removeClass("correct-answer")
+          }, 500);
+        }
+        else if (cAns.attr("data-win") === "loose") {
+          cAns.attr("data-win", "");
+          gameEnv.incorrect++;
+          gameEnv.roundIncorrect++;
+          $("#question").text("Wrong!");
+          cAns.addClass("wrong-answer");
+          setTimeout(function () {
+            cAns.removeClass("wrong-answer")
+          }, 500);
+        }
+        else {
+          return;
+        }
+        clearInterval(gameEnv.clock.clockInterval);
+        gameEnv.number++;
+        if (gameEnv.number < 16) {
+          $("#number").text(`#: ${gameEnv.number}`);
+        }
+        console.log("correct");
+        $("#answer-feild").fadeOut(500, function () {
+          gameEnv.loadQuestion();
+        });
+        $("li").removeEventListener();
       });
-      $("li").removeEventListener();
-    });
+    }
   },
 
   shuffle: function(array) {
@@ -357,14 +326,15 @@ window.gameEnv = {
     });
   },
 
-  randomBackground: function(theme) {
-    $.ajax({
-      url: gameEnv.imgApi + "count=1",
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-    });
-  }
+  // Implement later
+  // randomBackground: function(theme) {
+  //   $.ajax({
+  //     url: gameEnv.imgApi + "count=1",
+  //     method: "GET"
+  //   }).then(function (response) {
+  //     console.log(response);
+  //   });
+  // }
 };
 
 $(document).ready(gameEnv.initialize());
@@ -375,7 +345,7 @@ function htmlFixer(string) {
   ret = ret.replace(/&#039;/g, "'");
   ret = ret.replace(/&oacute;/g, "o");
   ret = ret.replace(/&iacute;/g, "i");
-  ret = ret.replace(/&rsquo;/g, "'");
+  ret = ret.replace(/&rsquo;/g, "`");
   return ret;
 
 }
